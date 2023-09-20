2dfff0d0bbdd5c9436bfc04614fa63f8c6024061
Jose María Flores Zazo <jmfloreszazo@gmail.com>
2023-09-20 11:23:39 +0200

    Sample
    Sample

---
+++ src/AzureAIOpenAISemanticKernel/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.SemanticFunctions;
+
+namespace AzureAIOpenAISemanticKernel;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+        var kernel = Kernel.Builder
+            .WithAzureChatCompletionService(
+                config.GetSection("DeploymentName").Value,
+                config.GetSection("Endpoint").Value,
+                config.GetSection("apiKey").Value
+            )
+            .Build();
+
+        //For example:  I want a c# program for an user login with active directory
+
+        // Sample 1
+        Console.Write("Enter requirements: ");
+        var requirements = Console.ReadLine();
+        var prompt = $"""
+            Write a program with the following requirements:
+            {requirements}
+            """;
+
+        var promptConfig = new PromptTemplateConfig
+        {
+            Completion =
+            {
+                MaxTokens = 1000, Temperature = 0.2, TopP = 0.5
+            }
+        };
+
+        var promptTemplate = new PromptTemplate(
+            prompt, promptConfig, kernel
+        );
+
+        var functionConfig = new SemanticFunctionConfig(promptConfig, promptTemplate);
+
+        var function = kernel.RegisterSemanticFunction("WriteMyCode", functionConfig);
+
+        var outputCode = await kernel.RunAsync(function);
+
+        Console.WriteLine(outputCode);
+
+        // Sample 2
+        //Console.Write("Enter requirements: ");
+        //string? requirements = Console.ReadLine();
+
+        //var plugin = kernel.ImportSemanticSkillFromDirectory("Plugins", "WriteMyCode");
+
+        //var variables = new ContextVariables();
+        //variables.Set("requirements", requirements);
+
+        //var stepOne = await kernel.RunAsync(variables, plugin["Step1"]);
+
+        //Console.WriteLine(stepOne);
+
+    }
+}
